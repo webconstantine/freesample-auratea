ad14cec112af03725ed6d133f235878c07783128
Sarmad <mail@sarmadgardezi.com>
2026-07-09 17:45:42 +0500

    Update PremiumPackages product card to Daily Aura

--- src/components/PremiumPackages.tsx
+++ src/components/PremiumPackages.tsx
@@ -8,16 +8,18 @@ export default function PremiumPackages() {
           <hr className="divider center" />
         </div>
         <div className="prod-single">
-          <div className="card">
-            <a className="thumb" href="/free-sample">
-              <img src="/products/aurasampling2.jpeg" alt="Free Sample" />
+          <div className="card custom-prod-card">
+            <a className="thumb" href="https://aurapremiumtea.com/products/daily-aura-85g">
+              <img src="/products/daily-aura-85g.webp" alt="Daily Aura-85G" />
             </a>
-            <h3><a href="/free-sample">Free Sample</a></h3>
-            <div className="price"><span className="now">Free</span></div>
-            <a className="btn btn-outline" href="/free-sample">Get Free Sample</a>
+            <h3 className="prod-title">
+              <a href="https://aurapremiumtea.com/products/daily-aura-85g">Daily Aura-85G</a>
+            </h3>
+            <div className="prod-price">Rs.195.00</div>
           </div>
         </div>
       </div>
     </section>
   )
 }
+
